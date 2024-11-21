1d77e0ceba75583fc7e4f46dad28f32a4e818d84
Serch102799 <14565@utcv.edu.mx>
2024-11-20 18:49:18 -0600

    Implementacion de OAth 2.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,23 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { Capacitor } from '@capacitor/core';
+
+
+if (Capacitor.getPlatform() === 'web') {
+  console.log('Estás en una plataforma web');
+}
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'aplicacion',
-  webDir: 'www'
+  appId: 'com.eCocina.id',
+  appName: 'eCocina',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '898105310904-28gnb2p119q7pof1ebuf1dhg0en6db1u.apps.googleusercontent.com', // Tu Client ID
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { Capacitor } from '@capacitor/core'; 
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,11 +13,37 @@ import { Component, OnInit } from '@angular/core';
   imports: [IonicModule, CommonModule],
 })
 export class LoginPage implements OnInit {
-  constructor(private router: Router) {}
+  user: any;
 
-  ngOnInit() {}
+  constructor() {}
 
-  onClickIngresar() {
-    this.router.navigate(['/principal']);
+  ngOnInit() {
+    if (Capacitor.getPlatform() === 'web') {
+      GoogleAuth.initialize({
+        clientId: '898105310904-28gnb2p119q7pof1ebuf1dhg0en6db1u.apps.googleusercontent.com',  // Tu Client ID
+        scopes: ['profile', 'email'],
+      })
+      .then(() => console.log('GoogleAuth initialized successfully'))
+      .catch(err => console.error('Error initializing GoogleAuth', err));
+    }
   }
-}
+
+  async onClickIngresar() {
+    console.log('Inicio de sesión estándar');
+  }
+
+  async onGoogleSignIn() {
+    try {
+      const user = await GoogleAuth.signIn();
+      console.log('Usuario autenticado con Google:', user);
+      alert(`Bienvenido, ${user.name}`);
+    } catch (error: any) {
+      console.error('Error al iniciar sesión con Google:', error.message);
+      if (error.message.includes('signIn')) {
+        alert('Parece que GoogleAuth no está inicializado correctamente.');
+      } else {
+        alert('Ocurrió un error. Por favor, intenta nuevamente.');
+      }
+    }
+  }
+}  
